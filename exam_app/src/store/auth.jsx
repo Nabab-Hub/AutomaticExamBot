903b0e53a6b8e3fe4fc38490e70ec3b5b3958c47
SK Ahidulla <79365693+Nabab-Hub@users.noreply.github.com>
2024-11-05 23:01:33 +0530

    Update auth.jsx

--- exam_app/src/store/auth.jsx
+++ exam_app/src/store/auth.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext()
 export const AuthProvider = ({children}) => {
 
     // const BACKEND_HOSTING_URL = 'https://dbbe-2409-40e1-1d-e085-5d44-bc22-a559-944c.ngrok-free.app'
-    const BACKEND_HOSTING_URL = 'http://127.0.0.1:8000'
+    const BACKEND_HOSTING_URL = 'https://automatic-exam-bot.vercel.app'
 
     const [token, setToken] = useState(localStorage.getItem('token'))
     const [user, setUser] = useState('')
