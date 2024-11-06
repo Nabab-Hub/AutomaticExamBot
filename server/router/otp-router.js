8f316cc169975095216edf418055802cd76255b2
SK Ahidulla <79365693+Nabab-Hub@users.noreply.github.com>
2024-11-06 15:16:15 +0530

    Delete server directory

--- server/router/otp-router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {OtpForm, getOtp} = require('../controllers/otp-controller')
-
-router.route('/send_otp').post(OtpForm)
-router.route('/verify_otp').post(getOtp)
-
-module.exports = router;
